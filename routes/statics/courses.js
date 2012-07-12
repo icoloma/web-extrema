bd32c65cc47d836132c512988ccb526872551d21
Nacho Coloma <icoloma@extrema-sistemas.com>
2012-07-12 16:06:31 +0200

    Added las páginas de cursos de html5

--- routes/statics/courses.js
+++ routes/statics/courses.js
@@ -1,27 +1,7 @@
 module.exports = function (app) {
 
-  app.get('/courses/HTML5-es', function (req, res) {
-    res.render('statics/courses/HTML5-es', {title: 'Title'})
-  });
-
-  app.get('/courses/HTML5-en', function (req, res) {
-    res.render('statics/courses/HTML5-en', {title: 'Title'})
-  });
-
-  app.get('/courses/HTML5', function (req, res) {
-    res.render('statics/courses/HTML5-' + i18n.getLocale(req), {title: 'Title'})
-  });
-
-  app.get('/courses/HTML5-it', function (req, res) {
-    res.render('statics/courses/HTML5-it', {title: 'Title'})
-  });
-
-  app.get('/courses/javaspecialists', function (req, res) {
-    res.render('statics/courses/javaspecialists', {title: 'Title'})
-  });
-
-  app.get('/courses/spring', function (req, res) {
-    res.render('statics/courses/spring', {title: 'Title'})
+  app.get(/\/courses\/([^\/\.]+)$/, function (req, res) {
+    res.render('statics/courses/' + req.params[0], {title: __('Course-contents')})
   });
 
 }
