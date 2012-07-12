7f5b3f2d32d919a79ee079715e14409bc709c64a
José Roberto Vidal Madrid <rvidal@extrema-sistemas.com>
2012-07-12 17:45:48 +0200

    alineando descripcion del team, simplificando redirecciones

--- routes/statics/redirections.js
+++ routes/statics/redirections.js
@@ -1,37 +1,33 @@
 
-var redirect = function(server, old, newURL) {
-  server.get(old, function (req, res) {
-    res.redirect(newURL);
-  });
-}
 
 module.exports = function (server) {
 
-
-  server.get(/\/es\/.+/, function (req, res, next) {
-    res.cookie('language', 'es');
-    next();
-  });
-
-  redirect(server, '/es/formacion', '/courses');
-  redirect(server, '/training', '/courses');
-  redirect(server, '/es/formacion/curso-oficial-core-spring-0', '/courses/spring');
-  redirect(server, '/solutions', '/'); //
-  redirect(server, '/es/formacion/html5-y-css3', '/courses/html5');
-  redirect(server, '/training/html5-and-css3', '/courses/html5');
-  redirect(server, '/es/registro?cid=Curso Oficial Core Spring&conv=394', '/courses/spring');
-  redirect(server, '/training/spring-core-official-course', '/courses/spring');
-  redirect(server, '/es/formacion/javaspecialists-master-course', '/courses/javaspecialists');
-  redirect(server, '/es/registro?cid=Curso Oficial Core Spring&conv=405', '/courses/spring');
-  redirect(server, '/es/registro?cid=Enterprise Integration con Spring&conv=340', '/courses/spring');
-  redirect(server, '/clients', '/'); //
-  redirect(server, '/about-us', '/');; //
-  redirect(server, '/es/soluciones', '/'); //
-  redirect(server, '/es/equipo', '/team');
-  redirect(server, '/es/clientes', '/'); //
+  var redirect = function(old, newURL) {
+    server.get(old, function (req, res) {
+      if (/^\/es\//.test(old)) {
+        res.clearCookie('language');
+        res.cookie('language', 'es');
+      }
+      res.redirect(newURL);
+    });
+  };
 
 
-  server.get('/es/contacto', function (req, res) {
-    res.redirect('/contact')
-  });
+  redirect('/training', '/courses');
+  redirect('/es/formacion', '/courses');
+  redirect('/es/formacion/curso-oficial-core-spring-0', '/courses/spring');
+  redirect('/es/formacion/html5-y-css3', '/courses/html5');
+  redirect('/training/html5-and-css3', '/courses/html5');
+  redirect('/training/spring-core-official-course', '/courses/spring');
+  redirect('/es/formacion/javaspecialists-master-course', '/courses/javaspecialists');
+  redirect('/es/registro', '/courses');
+  redirect('/es/equipo', '/team');
+  redirect('/about-us', '/team');
+
+  // TODO: Hasta que tengamos los casos de éxito
+  redirect('/solutions', '/'); //
+  redirect('/es/soluciones', '/'); //
+  redirect('/clients', '/'); //
+  redirect('/es/clientes', '/'); //
+  redirect('/es/contacto', '/contact');
 };
