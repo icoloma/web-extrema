c2335fde1df2b8a7f7670a1d126b045e1f7252d9
Nacho Coloma <icoloma@gmail.com>
2014-09-23 16:40:55 +0200

    First try with magellan nav

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,13 @@ $(function () {
     tab: {
       deep_linking: true,
       scroll_to_content: false
+    },
+    "magellan-expedition": {
+      active_class: 'active', // specify the class used for active sections
+      threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+      destination_threshold: 20, // pixels from the top of destination for it to be considered active
+      throttle_delay: 50, // calculation throttling to increase framerate
+      fixed_top: 0, // top distance in pixels assigned to the fixed element on scroll
     }
   });
   
