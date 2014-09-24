84b407388be964d3601ddf6c3b71f25f8f91fe89
Nacho Coloma <icoloma@gmail.com>
2014-09-24 18:11:16 +0200

    Cambios en los cursos

--- src/js/training.js
+++ src/js/training.js
@@ -1,15 +1,32 @@
 $(function() {
-  var change = function(e) {
-    var $this = $(e.target)
-      , $nav = $this.parent()
-    ;
-    $this.closest('article').find('.js-contents').toggleClass('hide');
-    $nav.find('.js-hide-contents').toggleClass('hidden');
-    $nav.find('.js-show-contents').toggleClass('hidden');
-  };
-  $('.js-show-contents').click(change);
-  $('.js-hide-contents').click(change);
+  var $document = $(document)
 
+  , scrollTo = function($element) {
+    $('html, body').stop().animate({
+      'scrollTop': $element.offset().top - 100
+    });
+  }
+
+  // show/hide course contents
+  $document.on('click', '.js-hide-contents, .js-show-contents', function(e) {
+
+    var $article = $(e.currentTarget).closest('article')
+    , $contents = $article.find('.course-contents')
+    if (!$contents.height()) {
+      $contents.css('height', 500)
+      _.delay(function() {
+        $contents.css('height', '')
+      }, 500)
+    } else {
+      $contents.css('height', $contents.height())
+      _.defer(function() { $contents.css('height', 0) });
+    }
+    scrollTo($contents);
+    $article.find('.js-hide-contents, .js-show-contents').toggleClass('hidden');
+
+  });
+
+  // hide the subsidies container (fundacion tripartita etc) after 5 sec
   var $subsidized = $('.subsidized-container');
   if ($subsidized.length) {
     _.delay(function() {
