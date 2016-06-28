eeb51ed1ea01252433f3cde3dadc94e257630f56
Carlos Coloma <ccescribano@gmail.com>
2016-06-28 13:24:54 +0100

    Update courses Add new filter on courses page Style changes

--- _js/training.js
+++ _js/training.js
@@ -8,6 +8,9 @@ $(function() {
     }
   }
 
+  var filters = JSON.parse($('#filters').html());
+  var currentFilters = [];
+
   // show/hide course contents
   $document.on('click', '.js-hide-contents, .js-show-contents', function(e) {
 
@@ -33,23 +36,108 @@ $(function() {
 
   });
 
-  // hide the subsidies container (fundacion tripartita etc) after 5 sec
-  var $subsidized = $('.subsidized-container');
-  if ($subsidized.length) {
-    _.delay(function() {
-      $subsidized.addClass('collapsed')
-    }, 5000)
-    _.delay(function() {
-      $document.scrollTop($document.scrollTop() - $subsidized.outerHeight());
-      $subsidized.remove();
-      $('body').addClass('nested-f-topbar-fixed');
-    }, 6000)
+  var courses = $document.find('article').toArray();
+  var $filter = $document.find('.filter');
+  var $name = $filter.find('input');
+  var $noResultsFound = $document.find('.no-results');
+  var $results = $document.find('.courses');
+  var hasTag = function(course) {
+    var tags = course.dataset.labels.split(' ');
+    return currentFilters.every(function(filter) {
+      return tags.indexOf(filter) != -1;
+    })
+  }
+  var matchText = function(course, inputValue) {
+    var name = $(course).find('h1[itemprop=name]').html().toLowerCase();
+    var description = $(course).find('[itemprop=description]').html().toLowerCase();
+    return name.indexOf(inputValue) != -1 || description.indexOf(inputValue) != -1;
   }
 
-  // the hash is not working with topbar
-  $('.contain-to-grid.nested a').click(function(e) {
-    $('.contain-to-grid.nested .tab-title').removeClass('active');
-    $(this).parent().addClass('active');
+  var doFilter = function() {
+    var toHide = [];
+    var toShow = [];
+    var inputValue = $name.val().toLowerCase();
+
+    courses.forEach(function(course) {
+      // if there is no tags selected
+      if (currentFilters.length) {
+        if (hasTag(course) && matchText(course, inputValue)) {
+          toShow.push(course);
+        } else {
+          toHide.push(course);
+        }
+      } else {
+        if (matchText(course, inputValue)) {
+          toShow.push(course);
+        } else {
+          toHide.push(course);
+        }
+      }
+    });
+    if (!toShow.length) {
+      $results.addClass('hide');
+      $noResultsFound.removeClass('hide');
+    } else {
+      $(toShow).removeClass('hide');
+      $(toHide).addClass('hide');
+      $results.removeClass('hide');
+      $noResultsFound.addClass('hide');
+    }
+
+  }
+
+  $name.on('keyup', $.throttle(250, doFilter));
+
+  $filter.on('submit', 'form', function(e) {
+    e.preventDefault();
+    doFilter();
   });
+
+  // update courses with selected labels
+  $document.on('click', '.label', function(e) {
+    var $target = $(e.target);
+    // si se está activando
+    if (!$target.hasClass('active')) {
+      // if it not a multiple category, we have to deselect the another ones
+      var filter = filters[$target.data('name')];
+      if (!filter.multiple) {
+        // the categories list
+        var tmp = $target.parent().parent().find('.label');
+        tmp.removeClass('active');
+        filter.values.forEach(function(value) {
+          var index = currentFilters.indexOf(value);
+          index !== -1 && currentFilters.splice(index, 1);
+        })
+      }
+      currentFilters.push($target.data('value'));
+    } else {
+      // si se está desactivando
+      // se quita del filtro
+      currentFilters.splice(currentFilters.indexOf($target.data('value')), 1);
+    }
+    $target.toggleClass('active');
+
+    doFilter();
+  })
+
+  var query = window.location.search.substring(1);
+  if (query) {
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      if (pair[0] == 'q') {
+        $name.val(decodeURIComponent(pair[1]));
+      } else {
+        if (filters[pair[0]]) {
+          var parts = pair[1].split(',');
+          parts.forEach(function(part) {
+            currentFilters.push(part);
+            $filter.find('[data-value=' + part + ']').addClass('active');
+          })
+        }
+      }
+    }
+    doFilter();
+  }
 
 })
