c6fd8551169471169ce9b1cfa684d266f971b3ab
Carlos Coloma <ccescribano@gmail.com>
2018-08-13 16:05:20 +0100

    Do not automatically accept cookies on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
-import { Foundation } from 'foundation-sites/js/foundation.core';
+import { Foundation } from "foundation-sites/js/foundation.core";
 
 // TODO: Do we need foundation???
 Foundation.addToJquery($);
@@ -8,92 +8,100 @@ Foundation.addToJquery($);
 $(document).foundation();
 
 // COOKIES
-var cookieName = 'acceptscookies'
-  , accepts = undefined
-  , $window = $(window)
-  , initScrollPosition = $window.scrollTop()
-  , throttle = function(func, wait, options) {
-      var context, args, result;
-      var timeout = null;
-      var previous = 0;
-      if (!options) options = {};
-      var later = function() {
-        previous = options.leading === false ? 0 : new Date().getTime();
+var cookieName = "acceptscookies",
+  accepts = undefined,
+  $window = $(window),
+  throttle = function(func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function() {
+      previous = options.leading === false ? 0 : new Date().getTime();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+    return function() {
+      var now = new Date().getTime();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        clearTimeout(timeout);
         timeout = null;
+        previous = now;
         result = func.apply(context, args);
         if (!timeout) context = args = null;
-      };
-      return function() {
-        var now = new Date().getTime();
-        if (!previous && options.leading === false) previous = now;
-        var remaining = wait - (now - previous);
-        context = this;
-        args = arguments;
-        if (remaining <= 0 || remaining > wait) {
-          clearTimeout(timeout);
-          timeout = null;
-          previous = now;
-          result = func.apply(context, args);
-          if (!timeout) context = args = null;
-        } else if (!timeout && options.trailing !== false) {
-          timeout = setTimeout(later, remaining);
-        }
-        return result;
-      };
-    }
-  , analytics = function() {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  },
+  analytics = function() {
+    (function(i, s, o, g, r, a, m) {
+      i["GoogleAnalyticsObject"] = r;
+      (i[r] =
+        i[r] ||
+        function() {
+          (i[r].q = i[r].q || []).push(arguments);
+        }),
+        (i[r].l = 1 * new Date());
+      (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m);
+    })(
+      window,
+      document,
+      "script",
+      "//www.google-analytics.com/analytics.js",
+      "ga"
+    );
 
-      ga('create', 'UA-2309405-1', 'auto');
-      ga('send', 'pageview');
-    }
-;
+    ga("create", "UA-2309405-1", "auto");
+    ga("send", "pageview");
+  };
 
-$.each(document.cookie.split(';'), function(index, item) { 
-  const parts = $.trim(item).split('=');
+$.each(document.cookie.split(";"), function(index, item) {
+  const parts = $.trim(item).split("=");
   if (cookieName == parts[0]) {
     accepts = parts[1];
   }
 });
 
 if (!accepts) {
-  $('.cookie-container').removeClass('hide');
+  $(".cookie-container").removeClass("hide");
   var acceptCookies = function() {
-    document.cookie = cookieName + '=accepted;max-age=3153600000';
-    $('.cookie-container').hide(400);
+    document.cookie = cookieName + "=accepted;max-age=3153600000";
+    $(".cookie-container").hide(400);
     accepts = true;
     analytics();
-  }
-  $window.scroll(throttle(function() {
-    if (!accepts && Math.abs($window.scrollTop() - initScrollPosition) >= 400) {
-      acceptCookies();
-    }
-  }, 100));
-  $('.accept-cookies').on('click', acceptCookies);
+  };
+  $(".accept-cookies").on("click", acceptCookies);
 } else {
   analytics();
 }
 
-const $topBar = $('.top-bar');
-$topBar.find('.toggle-topbar').each((index, node) => {
-  node.addEventListener('click', () => {
-    $topBar.toggleClass('expanded');
-  })
-})
+const $topBar = $(".top-bar");
+$topBar.find(".toggle-topbar").each((index, node) => {
+  node.addEventListener("click", () => {
+    $topBar.toggleClass("expanded");
+  });
+});
 
 if (!Array.prototype.every) {
   Array.prototype.every = function(callbackfn, thisArg) {
-    'use strict';
+    "use strict";
     var T, k;
 
     if (this == null) {
-      throw new TypeError('this is null or not defined');
+      throw new TypeError("this is null or not defined");
     }
 
-    // 1. Let O be the result of calling ToObject passing the this 
+    // 1. Let O be the result of calling ToObject passing the this
     //    value as the argument.
     var O = Object(this);
 
@@ -103,7 +111,7 @@ if (!Array.prototype.every) {
     var len = O.length >>> 0;
 
     // 4. If IsCallable(callbackfn) is false, throw a TypeError exception.
-    if (typeof callbackfn !== 'function') {
+    if (typeof callbackfn !== "function") {
       throw new TypeError();
     }
 
@@ -117,23 +125,21 @@ if (!Array.prototype.every) {
 
     // 7. Repeat, while k < len
     while (k < len) {
-
       var kValue;
 
       // a. Let Pk be ToString(k).
       //   This is implicit for LHS operands of the in operator
-      // b. Let kPresent be the result of calling the HasProperty internal 
+      // b. Let kPresent be the result of calling the HasProperty internal
       //    method of O with argument Pk.
       //   This step can be combined with c
       // c. If kPresent is true, then
       if (k in O) {
-
         // i. Let kValue be the result of calling the Get internal method
         //    of O with argument Pk.
         kValue = O[k];
 
         // ii. Let testResult be the result of calling the Call internal method
-        //     of callbackfn with T as the this value and argument list 
+        //     of callbackfn with T as the this value and argument list
         //     containing kValue, k, and O.
         var testResult = callbackfn.call(T, kValue, k, O);
 
@@ -151,13 +157,11 @@ if (!Array.prototype.every) {
 // Production steps of ECMA-262, Edition 5, 15.4.4.18
 // Reference: http://es5.github.io/#x15.4.4.18
 if (!Array.prototype.forEach) {
-
   Array.prototype.forEach = function(callback, thisArg) {
-
     var T, k;
 
     if (this == null) {
-      throw new TypeError(' this is null or not defined');
+      throw new TypeError(" this is null or not defined");
     }
 
     // 1. Let O be the result of calling toObject() passing the
@@ -169,10 +173,10 @@ if (!Array.prototype.forEach) {
     // 3. Let len be toUint32(lenValue).
     var len = O.length >>> 0;
 
-    // 4. If isCallable(callback) is false, throw a TypeError exception. 
+    // 4. If isCallable(callback) is false, throw a TypeError exception.
     // See: http://es5.github.com/#x9.11
     if (typeof callback !== "function") {
-      throw new TypeError(callback + ' is not a function');
+      throw new TypeError(callback + " is not a function");
     }
 
     // 5. If thisArg was supplied, let T be thisArg; else let
@@ -186,7 +190,6 @@ if (!Array.prototype.forEach) {
 
     // 7. Repeat, while k < len
     while (k < len) {
-
       var kValue;
 
       // a. Let Pk be ToString(k).
@@ -196,7 +199,6 @@ if (!Array.prototype.forEach) {
       //    This step can be combined with c
       // c. If kPresent is true, then
       if (k in O) {
-
         // i. Let kValue be the result of calling the Get internal
         // method of O with argument Pk.
         kValue = O[k];
